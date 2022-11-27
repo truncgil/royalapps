5a298e3d632508abfb95d06e5a5d958f6b814a3c
Ümit Tunç <umitreva@gmail.com>
2022-11-27 09:09:11 +0300

    Add middleware control for token

--- royaleapps/app/Http/Kernel.php
+++ royaleapps/app/Http/Kernel.php
@@ -54,6 +54,7 @@ class Kernel extends HttpKernel
      * @var array<string, class-string|string>
      */
     protected $routeMiddleware = [
+        'require.token' => \App\Http\Middleware\EnsureTokenIsValid::class,
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
